252528e38be30806fc4b5b14283375d05af75fd9
Maksim Berjoza <torbjon@gmail.com>
2015-07-31 15:14:24 +0300

    version bump

--- infogram.php
+++ infogram.php
@@ -3,7 +3,7 @@
 	Plugin Name: Infogram
 	Plugin URI: https://infogr.am
 	Description: It allows you to insert graphics from the site www.infogr.am
-	Version: 1.2
+	Version: 1.2.1
 	Tags: infogram, shortcode, iframe, insert, rest api, json 
 */
 
@@ -12,7 +12,7 @@ add_action('admin_menu', 'infogr_add_pages');
 
 function infogr_add_pages() {
 	//create new top-level menu
-	add_options_page('Infogram v1.2', 'Infogram settings', 'level_0', 'infogram', 'infogr_page');
+	add_options_page('Infogram v1.2.1', 'Infogram settings', 'level_0', 'infogram', 'infogr_page');
 	//call register settings function
 	add_action('admin_init', 'register_infogr_settings');
 }
